de72fe02094b1e7ecb3a370087d74c77683aa98d
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-08-23 23:37:00 +0600

    comparison analysis done via min max instead of first and last_value functions

--- window function/value function/last_first_value.sql
+++ window function/value function/last_first_value.sql
@@ -1,6 +1,7 @@
 USE SalesDB
 
 -- Find the lowest and highest sales for each product
+-- Find the difference in sales between the current and the lowest sales
 SELECT 
     OrderID,
     ProductID,
@@ -9,5 +10,6 @@ SELECT
     MIN(Sales) OVER (PARTITION BY ProductID) LowestSales2,
     LAST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) HighestSales,
     FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales DESC) HighestSales2,
-    MAX(Sales) OVER (PARTITION BY ProductID) HighestSales3
+    MAX(Sales) OVER (PARTITION BY ProductID) HighestSales3,
+    Sales - FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales) SalesDifference
 FROM Sales.Orders
